3473c165e8df70ea076d3d99c48406fce54b8b30
Maxim Belsky <public.belsky@gmail.com>
2018-08-26 16:55:35 +0700

    Add redis service

---
+++ src/services/redis/index.ts
@@ -0,0 +1,61 @@
+import redis, { RedisClient } from 'redis';
+import { promisify } from 'util';
+
+class Client {
+
+  private readonly client: RedisClient;
+  private readonly commands: {
+    expire: (key: string, expireInSeconds: number) => Promise<number>;
+    get: (key: string) => Promise<string>;
+    lrange: (key: string, start: number, stop: number) => Promise<string[]>;
+    rpush: (key: string, values: any[]) => Promise<number>;
+    setex: (key: string, seconds: number, value: string) => Promise<'OK'>;
+  };
+
+  constructor() {
+    const client = redis.createClient({
+      enable_offline_queue: false,
+    });
+    client.on('error', error => console.error(`Redis client error: ${error}`) );
+
+    this.commands = {
+      expire: promisify(client.expire).bind(client),
+      get: promisify(client.get).bind(client),
+      lrange: promisify(client.lrange).bind(client),
+      rpush: promisify(client.rpush).bind(client),
+      setex: promisify(client.setex).bind(client),
+    };
+
+    this.client = client;
+  }
+
+  public get<T>(key: string): Promise<T> {
+    return this.commands.get(key)
+      .then(result => JSON.parse(result));
+  }
+
+  public lrange<T>(key: string, start: number = 0, stop: number = -1): Promise<T[]> {
+    return this.commands.lrange(key, start, stop)
+      .then(result => {
+        return result.map(text => JSON.parse(text));
+      });
+  }
+
+  public setex(key: string, seconds: number, value: any) {
+    return this.commands.setex(key, seconds, JSON.stringify(value));
+  }
+
+  public expire(key: string, expireInSeconds: number = 0) {
+    return this.commands.expire(key, expireInSeconds);
+  }
+
+  public rpush(key: string, values: any[]) {
+    return this.commands.rpush(key, values.map(item => JSON.stringify(item)));
+  }
+
+  public end() {
+    this.client.end(true);
+  }
+}
+
+export const redisClientInstance = new Client();
