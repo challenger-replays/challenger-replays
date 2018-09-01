cdd2c1e496eaf30e3cf8e4e4a3afc43c2e46dfce
Maxim Belsky <public.belsky@gmail.com>
2018-09-01 19:47:11 +0700

    Remove code duplication

--- src/videos/services/data-source/index.ts
+++ src/videos/services/data-source/index.ts
@@ -1,90 +1,118 @@
 import { redisClientInstance as cacheController } from '../../../services/redis';
 import { Snippet } from '../../types';
-import { channelIds, playlistsLatestItemsList, search, uploadsPlaylistsIdList } from '../youtube';
+import {
+  channelIds,
+  playlistsLatestItemsList,
+  search,
+  uploadsPlaylistsIdList,
+} from '../youtube';
 
 const KEY_FEED = 'challenger-replays/videos/feed';
 const KEY_SEARCH = 'challenger-replays/videos/query';
 
-const FEED_EXPIRE_TIME = parseInt(`${process.env.CACHE_FEED_EXPIRE_TIME}`, 10) || 5 * 60;
-const SEARCH_RESULTS_EXPIRE_TIME = parseInt(`${process.env.CACHE_SEARCH_RESULTS_EXPIRE_TIME}`, 10) || 1 * 60;
+const FEED_EXPIRE_TIME =
+  parseInt(`${process.env.CACHE_FEED_EXPIRE_TIME}`, 10) || 5 * 60;
+const SEARCH_RESULTS_EXPIRE_TIME =
+  parseInt(`${process.env.CACHE_SEARCH_RESULTS_EXPIRE_TIME}`, 10) || 1 * 60;
 
 export class DataSource {
-  private cacheQueue: {
+  private cacheQueues: {
     [key: string]: Array<{
-      resolve: (value: Snippet[] | PromiseLike<Snippet[]>) => void,
-      reject: (reason: any) => void,
+      resolve: (value: Snippet[] | PromiseLike<Snippet[]>) => void;
+      reject: (reason: any) => void;
     }>;
-  };
-
-  constructor() {
-    this.cacheQueue = {
-      feed: [],
-    };
-  }
+  } = {};
 
   public async getFeed(): Promise<Snippet[]> {
-    try {
-      const cache = await cacheController.get<Snippet[]>(KEY_FEED); // replace with list
-      if (cache) {
-        return cache;
-      }
-     } catch (e) {
-      console.error(e);
+    const cache = await this.getCachedSnippets(KEY_FEED);
+    if (cache) {
+      return cache;
     }
 
-    const queue = this.cacheQueue.feed;
-    const promise = new Promise<Snippet[]>((resolve, reject) => queue.push({resolve, reject}));
-
-    if (1 === queue.length) {
-      try {
+    return this.loadData({
+      expire: FEED_EXPIRE_TIME,
+      executor: async () => {
         const ids = await uploadsPlaylistsIdList(channelIds);
-        const latestVideos = await playlistsLatestItemsList(ids);
-
-        try {
-          await cacheController.setex(KEY_FEED, FEED_EXPIRE_TIME, latestVideos);
-        } catch (e) {
-          console.error(e);
-        }
-
-        queue.forEach(({ resolve }) => resolve(latestVideos));
-      } catch (e) {
-        queue.forEach(({ reject }) => reject(e));
-      } finally {
-        queue.splice(0); // remove all watchers
-      }
-    }
-
-    return promise;
+        return playlistsLatestItemsList(ids);
+      },
+      key: KEY_FEED,
+    });
   }
 
-  public async getSearchResult(query: string, offset = 0, resultsPerPage = 10): Promise<Snippet[]> {
+  public async getSearchResult(
+    query: string,
+    offset = 0,
+    resultsPerPage = 10,
+  ): Promise<Snippet[]> {
     const key = `${KEY_SEARCH}:${query}`;
     const end = offset + resultsPerPage;
 
+    const cache = await this.getCachedSnippets(key, offset, end);
+    if (cache) {
+      return cache;
+    }
+
+    return this.loadData({
+      expire: SEARCH_RESULTS_EXPIRE_TIME,
+      executor: () => {
+        return search(channelIds, query);
+      },
+      key,
+    });
+  }
+
+  private async getCachedSnippets(
+    key: string,
+    start: number = 0,
+    stop: number = -1,
+  ): Promise<Snippet[] | undefined> {
     try {
-      const cache = await cacheController.lrange<Snippet>(key, offset, end);
+      const cache = await cacheController.lrange<Snippet>(key, start, stop);
       if (cache && 0 < cache.length) {
         return cache;
       }
     } catch (e) {
-      console.error(e);
+      console.log(e);
     }
 
-    const queue = this.cacheQueue[key] || [];
-    const promise = new Promise<Snippet[]>((resolve, reject) => queue.push({resolve, reject}));
+    return undefined;
+  }
+
+  private async loadData(options: {
+    expire: number;
+    executor: () => Promise<Snippet[]>;
+    key: string;
+    start?: number;
+    stop?: number;
+  }): Promise<Snippet[]> {
+    const { expire, executor } = options;
+    const key = options.key.toLowerCase().trim();
+    const start = options.start || 0;
+    const stop = options.stop || -1;
+
+    const { cacheQueues } = this;
+    if (!cacheQueues.hasOwnProperty(key)) {
+      cacheQueues[key] = [];
+    }
+
+    const queue = cacheQueues[key];
+    const promise = new Promise<Snippet[]>((resolve, reject) => {
+      queue.push({ resolve, reject });
+    });
 
     if (1 === queue.length) {
       try {
-        const searchResult: Snippet[] = await search(channelIds, query);
+        const data = await executor();
 
         try {
-          await cacheController.rpush(key, searchResult);
-          await cacheController.expire(key, SEARCH_RESULTS_EXPIRE_TIME);
+          await cacheController.rpush(key, data);
+          await cacheController.expire(key, expire);
         } catch (e) {
           console.error(e);
         }
 
-        queue.forEach(({ resolve }) => resolve(searchResult.slice(offset, end)));
+        const result = data.slice(start, stop);
+        queue.forEach(({ resolve }) => resolve(result));
       } catch (e) {
         queue.forEach(({ reject }) => reject(e));
       } finally {
