5ab952d02ef6aa5e689bdeefaadd2e200ac6e716
Maxim Belsky <public.belsky@gmail.com>
2018-09-01 12:22:05 +0700

    Add api prefix

--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,9 @@
 import Router from 'koa-router';
 import videos from './videos/controllers';
 
-const router = new Router()
+const router = new Router({
+  prefix: '/api/v1',
+})
   .use('/videos', videos.routes());
 
 export default router;
