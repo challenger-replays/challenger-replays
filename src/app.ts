3183fe206cd704e0582629dd0b471971fad4bb0d
Maxim Belsky <public.belsky@gmail.com>
2018-09-02 13:17:17 +0700

    Add compression

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,9 @@
 import Koa from 'koa';
+import compress from 'koa-compress';
 import router from './routes';
 
 const app = new Koa()
+  .use(compress())
   .use(router.routes())
   .use(router.allowedMethods());
 
--- src/videos/controllers.ts
+++ src/videos/controllers.ts
@@ -5,13 +5,13 @@ const dataSource = new DataSource();
 
 async function feed(ctx: IRouterContext) {
   const latestVideos = await dataSource.getFeed();
-  ctx.body = JSON.stringify(latestVideos, null, 2);
+  ctx.body = JSON.stringify(latestVideos);
 }
 
 async function search(ctx: IRouterContext) {
   const { q: query, offset } = ctx.query;
   const searchResult = await dataSource.getSearchResult(query, offset);
-  ctx.body = JSON.stringify(searchResult, null, 2);
+  ctx.body = JSON.stringify(searchResult);
   ctx.set({
     Connection: 'keep-alive',
   });
