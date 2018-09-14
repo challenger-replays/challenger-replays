fbca7d1bf3602d7f38cf6fde092dc6c6f17f471b
Maxim Belsky <public.belsky@gmail.com>
2018-09-14 22:24:02 +0700

    Replace search icon with material

--- packages/client/src/components/svgs/Search.jsx
+++ packages/client/src/components/svgs/Search.jsx
@@ -6,24 +6,8 @@ export default function Search({ width = '100%', height = '100%' }) {
       xmlns="http://www.w3.org/2000/svg"
       width={width}
       height={height}
-      viewBox="0 0 30 30">
-      <circle
-        cx="13"
-        cy="13"
-        r="9"
-        fill="none"
-        stroke="currentColor"
-        strokeWidth="2"
-        strokeMiterlimit="10"
-      />
-      <path
-        fill="none"
-        stroke="currentColor"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeMiterlimit="10"
-        d="M26 26l-6.563-6.563"
-      />
+      viewBox="0 0 24 24">
+      <path d="M15.5 14h-.79l-.28-.27A6.471 6.471 0 0 0 16 9.5 6.5 6.5 0 1 0 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z" />
     </svg>
   );
 }
