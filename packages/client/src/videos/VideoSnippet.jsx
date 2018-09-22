16df463c91f540155a6a9a6dcd64b6e6fd118bd0
Maxim Belsky <public.belsky@gmail.com>
2018-09-22 14:14:15 +0700

    Replace ids with urls

--- packages/client/src/videos/VideoSnippet.jsx
+++ packages/client/src/videos/VideoSnippet.jsx
@@ -6,17 +6,12 @@ import { AChannel, AVideo } from './styled';
 class VideoSnippet extends React.Component {
   render() {
     const {
-      channelId,
-      channelTitle,
-      publishedAt,
-      title,
-      thumbnails,
-      videoId,
+      channelTitle, channelUrl, title, videoUrl,
     } = this.props.details;
     return (
       <div>
-        <Url href={videoId} title={title} StyledA={AVideo} />
-        <Url href={channelId} title={channelTitle} StyledA={AChannel} />
+        <Url href={videoUrl} title={title} StyledA={AVideo} />
+        <Url href={channelUrl} title={channelTitle} StyledA={AChannel} />
       </div>
     );
   }
@@ -26,10 +21,12 @@ VideoSnippet.propTypes = {
   details: PropTypes.shape({
     channelId: PropTypes.string.isRequired,
     channelTitle: PropTypes.string.isRequired,
+    channelUrl: PropTypes.string.isRequired,
     publishedAt: PropTypes.string.isRequired,
     title: PropTypes.string.isRequired,
     thumbnails: PropTypes.object.isRequired,
     videoId: PropTypes.string.isRequired,
+    videoUrl: PropTypes.string.isRequired,
   }).isRequired,
 };
 
