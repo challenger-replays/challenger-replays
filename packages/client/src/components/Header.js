ff06dd0459ee143dd3f712db1644b8c3f81a6ac0
Maxim Belsky <public.belsky@gmail.com>
2018-09-22 10:54:10 +0700

    Add some styled components

--- packages/client/src/components/Header.js
+++ packages/client/src/components/Header.js
@@ -3,3 +3,7 @@ import styled from 'styled-components';
 export const H1 = styled.h1`
   color: ${p => (p.primary ? p.theme.primary : p.theme.text)};
 `;
+
+export const H2 = styled.h2`
+  color: ${p => (p.primary ? p.theme.primary : p.theme.text)};
+`;
