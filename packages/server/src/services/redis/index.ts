e2f9e048d8c0f29b2806f71a4c888991c033ff65
Maxim Belsky <public.belsky@gmail.com>
2018-09-28 20:41:11 +0700

    Implement llen

--- packages/server/src/services/redis/index.ts
+++ packages/server/src/services/redis/index.ts
@@ -2,11 +2,11 @@ import redis, { RedisClient } from 'redis';
 import { promisify } from 'util';
 
 class Client {
-
   private readonly client: RedisClient;
   private readonly commands: {
     expire: (key: string, expireInSeconds: number) => Promise<number>;
     get: (key: string) => Promise<string>;
+    llen: (key: string) => Promise<number>;
     lrange: (key: string, start: number, stop: number) => Promise<string[]>;
     rpush: (key: string, values: any[]) => Promise<number>;
     setex: (key: string, seconds: number, value: string) => Promise<'OK'>;
@@ -16,11 +16,12 @@ class Client {
     const client = redis.createClient({
       enable_offline_queue: false,
     });
-    client.on('error', error => console.error(`Redis client error: ${error}`) );
+    client.on('error', error => console.error(`Redis client error: ${error}`));
 
     this.commands = {
       expire: promisify(client.expire).bind(client),
       get: promisify(client.get).bind(client),
+      llen: promisify(client.llen).bind(client),
       lrange: promisify(client.lrange).bind(client),
       rpush: promisify(client.rpush).bind(client),
       setex: promisify(client.setex).bind(client),
@@ -30,15 +31,21 @@ class Client {
   }
 
   public get<T>(key: string): Promise<T> {
-    return this.commands.get(key)
-      .then(result => JSON.parse(result));
+    return this.commands.get(key).then(result => JSON.parse(result));
   }
 
-  public lrange<T>(key: string, start: number = 0, stop: number = -1): Promise<T[]> {
-    return this.commands.lrange(key, start, stop)
-      .then(result => {
-        return result.map(text => JSON.parse(text));
-      });
+  public llen(key: string): Promise<number> {
+    return this.commands.llen(key);
+  }
+
+  public lrange<T>(
+    key: string,
+    start: number = 0,
+    stop: number = -1,
+  ): Promise<T[]> {
+    return this.commands.lrange(key, start, stop).then(result => {
+      return result.map(text => JSON.parse(text));
+    });
   }
 
   public setex(key: string, seconds: number, value: any) {
