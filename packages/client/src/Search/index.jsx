0f6f98b7b7358d198f0f33bbcb4242b863362155
Maxim Belsky <public.belsky@gmail.com>
2018-09-29 21:33:20 +0700

    Make search request

--- packages/client/src/Search/index.jsx
+++ packages/client/src/Search/index.jsx
@@ -5,21 +5,48 @@ import HeaderBlock from './HeaderBlock';
 import Logo from './Logo';
 
 class Search extends React.Component {
+  constructor(props) {
+    super(props);
+    const searchParams = new UrlSearchParams(props.location.search);
+    this.state = {
+      query: searchParams.get('q'),
+      limit: searchParams.get('limit'),
+      offset: searchParams.get('offset') || 0,
+    };
+  }
+
   onSearch = (query) => {
     this.props.history.push(`/search?q=${query}`);
   };
 
   componentDidMount() {
-    console.log('Make a search request');
+    const { query, limit, offset } = this.state;
+    const searchParams = new UrlSearchParams();
+    searchParams.set('q', query);
+    searchParams.set('limit', limit);
+    searchParams.set('offset', offset);
+    fetch(`/api/v1/videos/search?${searchParams.toString()}`)
+      .then(response => response.json())
+      .then(json => this.setState({ ...json }))
+      .catch(e => console.error(`An error occured: ${e}`));
   }
 
   render() {
-    const query = new UrlSearchParams(this.props.location.search).get('q');
+    const { query, snippets } = this.state;
     return (
-      <HeaderBlock>
-        <Logo />
-        <SearchComponent initial={{ query }} onSearch={this.onSearch} />
-      </HeaderBlock>
+      <div>
+        <HeaderBlock>
+          <Logo />
+          <SearchComponent initial={{ query }} onSearch={this.onSearch} />
+        </HeaderBlock>
+        {Boolean(snippets) && (
+          <ul>
+            {snippets.map(snippet => (
+              <li key={snippet.videoId}>{snippet.title}</li>
+            ))}
+          </ul>
+        )}
+      </div>
     );
   }
 }
