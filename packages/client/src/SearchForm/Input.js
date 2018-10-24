1115f4448d80d7ba16c439317f0460d135352b72
Maxim Belsky <public.belsky@gmail.com>
2018-10-24 22:50:58 +0700

    Define some inputs auto- attrs

--- packages/client/src/SearchForm/Input.js
+++ packages/client/src/SearchForm/Input.js
@@ -1,9 +1,13 @@
 import styled from 'styled-components';
 import { Input as CommonInput } from '../styled';
+import { trackableViewport } from '../hocs';
 
 const Input = styled(CommonInput).attrs({
   placeholder: 'Find champions, players and more...',
-  type: 'search',
+  type: ({ viewport }) => (viewport.isMobile ? 'search' : 'text'),
+  autocapitalize: ({ viewport }) => (viewport.isMobile ? 'none' : undefined),
+  autocomplete: ({ viewport }) => (viewport.isMobile ? 'off' : undefined),
+  autocorrect: ({ viewport }) => (viewport.isMobile ? 'off' : undefined),
 })`
   flex-grow: 1;
   flex-shrink: 0;
@@ -15,4 +19,4 @@ const Input = styled(CommonInput).attrs({
   }
 `;
 
-export default Input;
+export default trackableViewport(Input);
